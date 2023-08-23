605f1c9b0b3a7da533bf031b88b25fb1b65e4ece
Sebastián Cruz <sebastian_cruz@Sebastian-Cruz-Hakkoda-XQP0GK7CTX.local>
2023-08-22 22:13:46 -0600

    Added data source and picklist

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,23 @@
 import streamlit
+import pandas
+import ssl
+
+# fix for ssl issues on the csv file url
+ssl._create_default_https_context = ssl._create_unverified_context
 
 streamlit.title('My Parents New Healthy Dinner')
-
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.text('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruits_to_show)
